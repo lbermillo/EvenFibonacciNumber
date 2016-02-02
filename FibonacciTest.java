1a086dd226454241cc096216947ff830a7cfd318
lbermillo <lorenzo.bermillo@gmail.com>
2016-02-02 17:36:03 -0600

    initial commit

---
+++ FibonacciTest.java
@@ -0,0 +1,10 @@
+package efn;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FibonacciTest {
+    @Test
+    public void canaryTest () { assertTrue(true); }
+}
